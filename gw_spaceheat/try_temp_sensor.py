32d149046f8384041351462aa1991bec7531a21f
Jessica <jessica.lynn.millar@gmail.com>
2022-05-31 10:53:49 -0400

    try the 4th temp sensor

--- gw_spaceheat/try_temp_sensor.py
+++ gw_spaceheat/try_temp_sensor.py
@@ -8,7 +8,7 @@ if platform.platform() == 'Linux-4.19.118-v7l+-armv7l-with-glibc2.28':
 else:
     load_house.load_all(house_json_file='input_data/dev_house.json')
 
-node = ShNode.by_alias["a.tank.temp0"]
+node = ShNode.by_alias["a.tank.temp4"]
 
 (actor_function, keys) = strategy_switcher(node.python_actor_name)
 sensor = actor_function(node)
