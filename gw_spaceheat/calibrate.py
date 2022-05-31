a9ab382aac72d31ec5d74cc4d3707a73d9930283
Jessica <jessica.lynn.millar@gmail.com>
2022-05-31 08:49:05 -0400

    temp calibration spike

---
+++ gw_spaceheat/calibrate.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+import time
+
+
+
+def kill_sensors():
+    output = os.popen('ps -A  | grep try_multiple_temp_sensors').read()
+    possibles = str(output).split('\n')
+    for line in possibles:
+        if 'try_multiple_temp_sensors.py' in line:
+            pid = line.split(' ')[0]
+            os.system(f'kill -9 {pid}')
+            print(f'Killed try_multiple_temp_sensors process {pid}')
+
+kill_sensors()
+
+
+while True:
+    cmd = ['python', 'try_multiple_temp_sensors.py']
+    sensor_process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    print("sensors restarted")
+    time.sleep(20)
+    kill_sensors()
+    print("sensors killed)")
+    time.sleep(1)
