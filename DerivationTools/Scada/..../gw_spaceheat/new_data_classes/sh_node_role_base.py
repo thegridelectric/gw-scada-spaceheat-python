a1b3058ac0a073bab603fcf4be1b8b05fa01a1cd
Jessica <jessica.lynn.millar@gmail.com>
2022-06-07 13:51:10 -0400

    delete unused derivation files

--- DerivationTools/Scada/..../gw_spaceheat/new_data_classes/sh_node_role_base.py
+++
@@ -1,7 +0,0 @@
-""" ShNodeRole Base Class Definition """
-import time
-import uuid
-from typing import Optional
-from abc import ABC, abstractproperty
-from gw.mixin import StreamlinedSerializerMixin
-
