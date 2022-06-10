220e6fe97f5dd6a6e2035d2de8db6ebf5cad42db
stickler-ci <support@stickler-ci.com>
2022-06-10 22:09:58 +0000

    Fixing style errors.

--- test/test_examples.py
+++ test/test_examples.py
@@ -10,6 +10,8 @@ from actors.strategy_switcher import main as strategy_switcher
 import load_house
 
 # noinspection PyUnresolvedReferences
+
+
 def test_imports():
     """Verify modules can be imported"""
     # note: disable warnings about local imports
@@ -26,5 +28,6 @@ def test_load_house():
     assert len(nodes_w_components) == 19
     actor_nodes_w_components = list(filter(lambda x: x.python_actor_name is not None, nodes_w_components))
     assert len(actor_nodes_w_components) == 7
-    temp_sensor_nodes = list(filter(lambda x: isinstance(x.primary_component.cac, TempSensorCac), actor_nodes_w_components))
+    temp_sensor_nodes = list(filter(lambda x: isinstance(
+        x.primary_component.cac, TempSensorCac), actor_nodes_w_components))
     assert len(temp_sensor_nodes) == 5
