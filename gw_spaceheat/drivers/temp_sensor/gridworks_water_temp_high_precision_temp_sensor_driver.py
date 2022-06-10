33a62b6066cf6f90e57e8b9105c6e8b570f72270
Andrew Schweitzer <schweitz72@gmail.com>
2022-06-10 16:17:21 -0400

    Simple examples of pytest tests

--- gw_spaceheat/drivers/temp_sensor/gridworks_water_temp_high_precision_temp_sensor_driver.py
+++ gw_spaceheat/drivers/temp_sensor/gridworks_water_temp_high_precision_temp_sensor_driver.py
@@ -7,6 +7,9 @@ from schema.enums.make_model.make_model_map import MakeModel
 
 
 class GridworksWaterTempSensorHighPrecision_TempSensorDriver(TempSensorDriver):
+    BASE_READ_TIME_MS = 879
+    READ_TIME_FUZZ_MULTIPLIER = 6
+    MEAN_READ_TIME_MS = BASE_READ_TIME_MS + (READ_TIME_FUZZ_MULTIPLIER / 2)
 
     def __init__(self, component: TempSensorComponent):
         super(GridworksWaterTempSensorHighPrecision_TempSensorDriver, self).__init__(component=component)
@@ -14,7 +17,7 @@ class GridworksWaterTempSensorHighPrecision_TempSensorDriver(TempSensorDriver):
             raise Exception(f"Expected GridWorks__WaterTempHighPrecision, got {component.cac}")
 
     def read_temp(self):
-        reading_time_ms = 879 + int(6 * random.random())
+        reading_time_ms = self.BASE_READ_TIME_MS + int(self.READ_TIME_FUZZ_MULTIPLIER * random.random())
         temp_f_times_1000 = 67000 + int(500 * random.random())
         time.sleep(reading_time_ms / 1000)
         return temp_f_times_1000
