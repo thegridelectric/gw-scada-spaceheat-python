a3f7d4fa121d88fd3f4a5dc2918549df253905fe
Andrew Schweitzer <schweitz72@gmail.com>
2022-06-14 09:52:17 -0400

    Add test_collect_temp_data() (#34)
    * Add test_collect_temp_data()
    * Add PYTHONPATH to CI config. 
    * Remove test_temp_sensor_sends() and UniversalTestEar  in favor of test_collect_temp_data()
    
    Co-authored-by: stickler-ci <support@stickler-ci.com>
    Co-authored-by: Jessica <jessica.lynn.millar@gmail.com>

--- test/test_examples.py
+++ test/test_examples.py
@@ -1,6 +1,9 @@
 # This should be an absolute import from package base: "gw_spaceheat...."
 # That requires changes *all* imports to use absolute import, so we don't do this in this demo.
 import time
+import typing
+from collections import defaultdict
+
 from data_classes.cacs.temp_sensor_cac import TempSensorCac
 from data_classes.sh_node import ShNode
 import load_house
@@ -8,11 +11,25 @@ from actors.power_meter import PowerMeter
 from actors.primary_scada import PrimaryScada
 from actors.atn import Atn
 from actors.tank_water_temp_sensor import TankWaterTempSensor
-from universal_test_ear import UniversalTestEar
-from schema.gt.gt_telemetry.gt_telemetry_maker import GtTelemetry, GtTelemetry_Maker
+from schema.gs.gs_dispatch import GsDispatch
 from schema.gs.gs_pwr_maker import GsPwr_Maker
-# noinspection PyUnresolvedReferences
 
+
+class ScadaRecorder(PrimaryScada):
+    """Record data about a PrimaryScada execution during test"""
+
+    num_received: int
+    num_received_by_topic: typing.Dict[str, int]
+
+    def __init__(self, node: ShNode):
+        self.num_received = 0
+        self.num_received_by_topic = defaultdict(int)
+        super().__init__(node)
+
+    def on_mqtt_message(self, client, userdata, message):
+        self.num_received += 1
+        self.num_received_by_topic[message.topic] += 1
+        super().on_mqtt_message(client, userdata, message)
 
 def test_imports():
     """Verify modules can be imported"""
@@ -38,25 +55,6 @@ def test_load_house():
     assert len(temp_sensor_nodes) == 5
 
 
-def test_temp_sensor_sends():
-    load_house.load_all(house_json_file='../test/test_data/test_load_house.json')
-
-    t0_node = ShNode.by_alias["a.tank.temp0"]
-    t0 = TankWaterTempSensor(node=t0_node)
-    t0.terminate_sensing()
-    t0.sensing_thread.join()
-    ear = UniversalTestEar()
-    ear.client.loop_start()
-    payload = GtTelemetry_Maker(name=t0.telemetry_name,
-                                value=int(t0.temp),
-                                exponent=0,
-                                scada_read_time_unix_ms=int(time.time() * 1000)).tuple
-    t0.publish(payload=payload)
-    time.sleep(.4)
-    assert isinstance(ear.latest_payload, GtTelemetry)
-    assert ear.latest_from_node == t0_node
-
-
 def test_async_power_metering_dag():
     load_house.load_all(house_json_file='../test/test_data/test_load_house.json')
     meter_node = ShNode.by_alias["a.m"]
@@ -75,3 +73,17 @@ def test_async_power_metering_dag():
     meter.publish(payload=payload)
     time.sleep(.3)
 #     assert atn.total_power_w == 2100
+
+
+def test_collect_temp_data():
+    """Verify Scada receives publication from TankWaterTempSensor"""
+    load_house.load_all(house_json_file='../test/test_data/test_load_house.json')
+    scada = ScadaRecorder(node=typing.cast(ShNode, ShNode.by_alias["a.s"]))
+    thermo = TankWaterTempSensor(node=typing.cast(ShNode, ShNode.by_alias["a.tank.temp0"]))
+    time.sleep(1)
+    thermo.terminate_sensing()
+    thermo.sensing_thread.join()
+    scada.terminate_scheduling()
+    scada.schedule_thread.join()
+    assert scada.num_received > 0
+    assert scada.num_received_by_topic["a.tank.temp0/gt.telemetry.110"] == scada.num_received
