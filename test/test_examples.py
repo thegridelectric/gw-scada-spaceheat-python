4bfb0bb86625da363da50aa661c7eab297bd002f
stickler-ci <support@stickler-ci.com>
2022-06-25 22:46:07 +0000

    Fixing style errors.

--- test/test_examples.py
+++ test/test_examples.py
@@ -581,11 +581,11 @@ def test_electric_meter_cac():
 def test_electric_meter_component():
     load_house.load_all()
     d = {
-            "ComponentId": "2bfd0036-0b0e-4732-8790-bc7d0536a85e",
-            "DisplayName": "Main Power meter for Little orange house garage space heat",
-            "ComponentAttributeClassId": "28897ac1-ea42-4633-96d3-196f63f5a951",
-            "HwUid": "9999"
-        }
+        "ComponentId": "2bfd0036-0b0e-4732-8790-bc7d0536a85e",
+        "DisplayName": "Main Power meter for Little orange house garage space heat",
+        "ComponentAttributeClassId": "28897ac1-ea42-4633-96d3-196f63f5a951",
+        "HwUid": "9999"
+    }
 
     gw_tuple = GtElectricMeterComponent_Maker.dict_to_tuple(d)
     assert gw_tuple.ComponentId in ElectricMeterComponent.by_id.keys()
