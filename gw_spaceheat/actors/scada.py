573628ee5389740e54318e932a5ea09ce538ad89
Jessica <jessica.lynn.millar@gmail.com>
2022-07-04 11:28:58 -0400

    stickler fix

--- gw_spaceheat/actors/scada.py
+++ gw_spaceheat/actors/scada.py
@@ -235,10 +235,10 @@ class Scada(ScadaBase):
     def gt_driver_booleanactuator_cmd_record_received(
         self, from_node: ShNode, payload: GtDriverBooleanactuatorCmd
     ):
-        """ The boolean actuator actor reports when it has sent an actuation command
-        to its driver. We add this to information to be sent up in the 5 minute status 
+        """The boolean actuator actor reports when it has sent an actuation command
+        to its driver. We add this to information to be sent up in the 5 minute status
         package.
-   
+
         This is different than reporting a _reading_ of the state of the
         actuator. Note that a reading of the state of the actuator may not mean the relay
         is in the read position. For example, the NCD relay requires two power sources - one
@@ -251,7 +251,7 @@ class Scada(ScadaBase):
         the boost element turning on. Or the element could be burned out.
 
         So measuring the current and/or power of the thing getting
-        actuated is really the best test.  """
+        actuated is really the best test."""
 
         if from_node not in self.my_boolean_actuators():
             raise Exception("boolean actuator command records must come from boolean actuator")
