2ca0e461d2d24ba46327f0dcbf18409f0f60913d
Andrew Schweitzer <schweitz72@gmail.com>
2022-10-20 16:20:51 -0400

    Add tests/atn

---
+++ tests/atn/__init__.py
@@ -0,0 +1,11 @@
+from tests.atn.atn import Atn2
+
+get_atn = Atn2.get_atn
+
+from tests.atn.atn_config import AtnSettings
+
+__all__ = [
+    "Atn2",
+    "AtnSettings",
+    "get_atn",
+]
---
+++ tests/atn/atn_config.py
@@ -0,0 +1,36 @@
+from typing import Optional, Any
+
+from pydantic import BaseSettings, validator
+
+from config import MQTTClient
+from config import Paths
+from config import LoggingSettings
+
+DEFAULT_NAME = "atn"
+
+class AtnSettings(BaseSettings):
+    scada_mqtt: MQTTClient = MQTTClient()
+    paths: Paths = None
+    logging: LoggingSettings = None
+
+    class Config:
+        env_prefix = "ATN_"
+        env_nested_delimiter = "__"
+
+    @validator("paths", always=True)
+    def get_paths(cls, v: Optional[Paths]) -> Paths:
+        if v is None:
+            v = Paths(name=DEFAULT_NAME)
+        return v
+
+    @validator("logging", always=True)
+    def get_logging(cls, v: Optional[LoggingSettings], values: dict[str, Any]) -> LoggingSettings:
+        if v is None:
+            v = LoggingSettings()
+        if not isinstance(values["paths"], Paths):
+            raise ValueError(f"ERROR. 'paths' value has type {type(values['paths'])}, not Paths")
+        paths: Paths = values["paths"]
+        v.base_log_name = str(paths.name)
+        v.file_handler.filename = "atn.log"
+        return v
+
