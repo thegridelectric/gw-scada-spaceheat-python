09b9c067e3da5a4325c52441e09a0e8a23cc8a3e
Andrew Schweitzer <schweitz72@gmail.com>
2023-03-01 16:13:11 -0500

    EZFlo tolerates errors better and uses regex to parse device output

--- gw_spaceheat/drivers/pipe_flow_sensor/atlas_ezflo__pipe_flow_sensor_driver.py
+++ gw_spaceheat/drivers/pipe_flow_sensor/atlas_ezflo__pipe_flow_sensor_driver.py
@@ -1,5 +1,7 @@
 import sys
 import time
+import re
+from typing import Optional
 
 from result import Err
 from result import Ok
@@ -39,8 +41,11 @@ class EZFlowI2cAddressMissing(DriverWarning):
         return s
 
 class AtlasEzflo_PipeFlowSensorDriver(PipeFlowSensorDriver):
-    dev: AtlasI2C
+    SUCCESS_FLOAT_REGEX = re.compile("Success.*:(?P<float>[+-]? *(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?)")
+
+    dev: Optional[AtlasI2C]
     _last_cumulative_gallons: int = 0
+
 
     def __init__(self, component: PipeFlowSensorComponent, settings: ScadaSettings):
         super(AtlasEzflo_PipeFlowSensorDriver, self).__init__(component=component, settings=settings)
@@ -48,62 +53,77 @@ class AtlasEzflo_PipeFlowSensorDriver(PipeFlowSensorDriver):
             raise Exception(f"Expected {MakeModel.ATLAS__EZFLO}, got {component.cac}")
 
     def start(self) -> Result[DriverResult[bool], Exception]:
-        device = AtlasI2C()
-        device_address = self.component.i2c_address
-        device.set_i2c_address(device_address)
-        response = device.query("I")
         driver_result = DriverResult(True)
+        device_address = self.component.i2c_address
         try:
-            moduletype = response.split(",")[1]
-            name = device.query("name,?").split(",")[1]
-        except IndexError:
-            moduletype = ""
-            name = ""
+            device = AtlasI2C()
+            device.set_i2c_address(device_address)
+            response = device.query("I")
+            try:
+                moduletype = response.split(",")[1]
+                name = device.query("name,?").split(",")[1]
+            except IndexError:
+                moduletype = ""
+                name = ""
+                driver_result.warnings.append(
+                    EZFlowI2cAddressMissing(
+                        device_address,
+                        msg=(
+                            f">> WARNING: device at I2C address {device_address} "
+                            " has not been identified as an EZO device, and will not be queried"
+                        )
+                    ).with_traceback(sys.exc_info()[2])
+                )
+            self.dev = AtlasI2C(address=device_address, moduletype=moduletype, name=name)
+            self._last_cumulative_gallons = 0
+            cum_read = self.read_cumulative_gallons()
+            if not cum_read.is_ok():
+                return Err(
+                    Problems(
+                        msg=f"ERROR in start()/read_cumulative_gallons() for {self.component.display_name}",
+                        warnings=driver_result.warnings,
+                        errors=[cum_read.err()]
+                    )
+                )
+            elif cum_read.value.value is not None:
+                self._last_cumulative_gallons = cum_read.value.value
+            driver_result.warnings.extend(cum_read.value.warnings)
+        except OSError as e:
             driver_result.warnings.append(
                 EZFlowI2cAddressMissing(
                     device_address,
-                    msg=(
-                        f">> WARNING: device at I2C address {device_address} "
-                        " has not been identified as an EZO device, and will not be queried"
-                    )
+                    msg=f"Startup failure for {device_address}:  {type(e)}  <{e}>"
                 ).with_traceback(sys.exc_info()[2])
             )
-        self.dev = AtlasI2C(address=device_address, moduletype=moduletype, name=name)
-        self._last_cumulative_gallons = 0
-        cum_read = self.read_cumulative_gallons()
-        if not cum_read.is_ok():
-            return Err(
-                Problems(
-                    msg=f"ERROR in start()/read_cumulative_gallons() for {self.component.display_name}",
-                    warnings=driver_result.warnings,
-                    errors=[cum_read.err()]
-                )
-            )
-        elif cum_read.value.value is not None:
-            self._last_cumulative_gallons = cum_read.value.value
-        driver_result.warnings.extend(cum_read.value.warnings)
+            self.dev = None
         return Ok(driver_result)
 
     def __repr__(self):
         return f"Atlas Ezflo driver for {self.component.display_name}"
 
     def read_cumulative_gallons(self) -> Result[DriverResult[int | None], Exception]:
-        time.sleep(1)
-        self.dev.write("R")
-        time.sleep(0.3)
-        v0 = self.dev.read()
         driver_result = DriverResult(None)
-        try:
-            val = float(v0.split(':')[1].split('\x00')[0])
-        except Exception as e:
-            driver_result.warnings.append(EZFlowI2cReadWarning(
-                f"<{e}> {type(e)}").with_traceback(sys.exc_info()[2]))
-        else:
-            driver_result.value = val
+        if self.dev is not None:
+            time.sleep(1)
+            self.dev.write("R")
+            time.sleep(0.3)
+            v0 = self.dev.read()
+            try:
+                if match := self.SUCCESS_FLOAT_REGEX.match(v0):
+                    val = float(match.group("float"))
+                    driver_result.value = val
+                else:
+                    driver_result.warnings.append(
+                        EZFlowI2cReadWarning(
+                            f"Float regex <{self.SUCCESS_FLOAT_REGEX.pattern}> failed to match <{v0}>")
+                    )
+            except Exception as e:
+                driver_result.warnings.append(EZFlowI2cReadWarning(
+                    f"<{e}> {type(e)}  raw read:<{v0}>").with_traceback(sys.exc_info()[2]))
         return Ok(driver_result)
 
     def read_telemetry_value(self) -> Result[DriverResult[int | None], Exception]:
         cum_result = self.read_cumulative_gallons()
         if cum_result.is_ok() and cum_result.value.value is not None:
-            cum_result.value = int(100 * cum_result.value.value * self.component.conversion_factor)
+            cum_result.value.value = int(100 * cum_result.value.value * self.component.conversion_factor)
         return cum_result
