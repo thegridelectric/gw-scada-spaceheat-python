2efb0f61a82cfbac81c7960646de4df9d9935753
Andrew Schweitzer <schweitz72@gmail.com>
2023-03-09 17:43:57 -0500

    Remove proactor code and get it from gridworks-protocol library

--- gw_spaceheat/gwproactor_test/dummies/child/__init__.py
+++
@@ -1,7 +0,0 @@
-from gwproactor_test.dummies.child.dummy import DummyChild
-from gwproactor_test.dummies.child.config import DummyChildSettings
-
-__all__ = [
-    "DummyChild",
-    "DummyChildSettings",
-]
--- gw_spaceheat/gwproactor_test/dummies/parent/__init__.py
+++
@@ -1,7 +0,0 @@
-from gwproactor_test.dummies.parent.dummy import DummyParent
-from gwproactor_test.dummies.parent.config import DummyParentSettings
-
-__all__ = [
-    "DummyParent",
-    "DummyParentSettings",
-]
