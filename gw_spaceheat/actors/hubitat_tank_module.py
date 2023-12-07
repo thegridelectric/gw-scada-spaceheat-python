thegridelectric/gw-scada-spaceheat-python

commit 1212f57d4099f71460bf5c9ae02c038047a22fe9

HubitatTankModule and HubitatPoller use scada name in error/warning reports

gw_spaceheat/actors/hubitat_tank_module.py
@@ -173,12 +173,11 @@ class FibaroTankTempPoller(RESTPoller):
     def __init__(
             self,
             name: str,
-            tank_module_name: str,
             sensor_supply_voltage: float,
             settings: FibaroTempSensorSettings,
             services: ServicesInterface
     ):
-        self._report_src = tank_module_name
+        self._report_src = services.name
         self._report_dst = services.name
         self._max_voltage = sensor_supply_voltage
         self._settings = settings
@@ -260,7 +259,6 @@ class HubitatTankModule(Actor):
         self._pollers = [
             FibaroTankTempPoller(
                 name=device.node_name,
-                tank_module_name=self.name,
                 settings=device,
                 services=services,
                 sensor_supply_voltage=self._component.sensor_supply_voltage

gw_spaceheat/actors/hubitat_poller.py
@@ -191,7 +191,7 @@ class HubitatRESTPoller(RESTPoller):
                         Payload=Problems(warnings=warnings).problem_event(
                             summary=(
                                 f"<{self._name}> _convert() warnings "
-                            ), src=self._name
+                            ), src=self._report_dst
                         )
                     )
                 )
@@ -201,7 +201,7 @@ class HubitatRESTPoller(RESTPoller):
                     Payload=Problems(errors=[e]).problem_event(
                         summary=(
                             f"<{self._name}> _convert() error"
-                        ), src=self._name
+                        ), src=self._report_dst
                     )
                 )
             )

commit 90dff03869feb0e8a4319525bfdb43a0ac2bf7fd

Fix loss tank reports caused by 1212f57

gw_spaceheat/actors/hubitat_tank_module.py
@@ -173,11 +173,12 @@ class FibaroTankTempPoller(RESTPoller):
     def __init__(
             self,
             name: str,
+            report_src: str,
             sensor_supply_voltage: float,
             settings: FibaroTempSensorSettings,
             services: ServicesInterface
     ):
-        self._report_src = services.name
+        self._report_src = report_src
         self._report_dst = services.name
         self._max_voltage = sensor_supply_voltage
         self._settings = settings
@@ -226,7 +227,7 @@ class FibaroTankTempPoller(RESTPoller):
                     Payload=Problems(errors=[e]).problem_event(
                         summary=(
                             f"Fibaro {self._settings.node_name} _convert() error "
-                        ), src=self._report_src
+                        )
                     )
                 )
             )
@@ -259,6 +260,7 @@ class HubitatTankModule(Actor):
         self._pollers = [
             FibaroTankTempPoller(
                 name=device.node_name,
+                report_src=self.name,
                 settings=device,
                 services=services,
                 sensor_supply_voltage=self._component.sensor_supply_voltage