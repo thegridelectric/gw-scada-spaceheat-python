16f32f3f5ad664fc85514218d867b92d53118f1a
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-10-03 11:14:36 -0400

    Fix ConfigList reference for analog-temp

--- gw_spaceheat/drivers/multipurpose_sensor/gridworks_tsnap1__multipurpose_sensor_driver.py
+++ gw_spaceheat/drivers/multipurpose_sensor/gridworks_tsnap1__multipurpose_sensor_driver.py
@@ -208,7 +208,7 @@ class GridworksTsnap1_MultipurposeSensorDriver(MultipurposeSensorDriver):
                 f"Expected make model in {models}, got {component.cac.MakeModel}"
             )
         c = component.gt
-        self.terminal_block_idx_list = [tc.TerminalBlockIdx for tc in c.ThermistorConfigList]
+        self.terminal_block_idx_list = [tc.TerminalBlockIdx for tc in c.ConfigList]
         self.telemetry_name_list = component.cac.TelemetryNameList
         self.ads_address = {i: address for i, address in enumerate(component.cac.AdsI2cAddressList)}
 
