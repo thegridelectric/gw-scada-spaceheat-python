cb18f217b8047cb392e89ddd8d1fd05c4b596683
Andrew Schweitzer <schweitz72@gmail.com>
2024-10-15 17:22:35 -0400

    dashboard: fix thermostat_names()

--- tests/atn/atn_config.py
+++ tests/atn/atn_config.py
@@ -22,17 +22,16 @@ class DashboardSettings(BaseModel):
     hack_hp: HackHpSettings = HackHpSettings()
 
     @classmethod
-    def thermostat_names(cls, node_names: list[str]) -> list[str]:
-        name_to_human_name :dict[str, str] = {}
-        thermostat_name_pattern = re.compile(
-            r"^zone(?P<zone_number>\d)-(?P<human_name>.*)$"
+    def thermostat_names(cls, channel_names: list[str]) -> list[str]:
+        thermostat_channel_name_pattern = re.compile(
+            r"^zone(?P<zone_number>\d)-(?P<human_name>.*)-(temp|set|state)$"
         )
-        for node_name in node_names:
-            if match := thermostat_name_pattern.match(node_name):
-                name_to_human_name[node_name] = match.group("human_name")
-        return [
-            name_to_human_name[name] for name in sorted(name_to_human_name.keys())
-        ]
+        thermostat_human_names = []
+        for channel_name in channel_names:
+            if match := thermostat_channel_name_pattern.match(channel_name):
+                if (human_name := match.group("human_name")) not in thermostat_human_names:
+                    thermostat_human_names.append(human_name)
+        return thermostat_human_names
 
 class AtnSettings(ProactorSettings):
     scada_mqtt: MQTTClient = MQTTClient()
