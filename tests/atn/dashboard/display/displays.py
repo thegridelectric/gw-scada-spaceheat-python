c6f70b2a5363b14c367ab0b87fa31423c303c7d6
Andrew Schweitzer <schweitz72@gmail.com>
2024-10-16 13:33:16 -0400

    dashboard: rename ascii_picture.py to picture.py

--- tests/atn/dashboard/display/displays.py
+++ tests/atn/dashboard/display/displays.py
@@ -9,7 +9,7 @@ from tests.atn.atn_config import DashboardSettings
 from tests.atn.dashboard.display.odds_and_ends import OddsAndEnds
 from tests.atn.dashboard.display.power import PowerDisplay
 from tests.atn.dashboard.display.thermostats import ThermostatDisplay
-from tests.atn.dashboard.display.ascii_picture import AsciiPicture
+from tests.atn.dashboard.display.picture import AsciiPicture
 from tests.atn.dashboard.channels.containers import Channels
 from tests.atn.dashboard.hackhp import HackHpStateCapture
 
@@ -17,7 +17,7 @@ class Displays:
     odds_and_ends: OddsAndEnds
     thermostat: ThermostatDisplay
     power: PowerDisplay
-    ascii_picture: AsciiPicture
+    picture: AsciiPicture
 
     def __init__(
             self,
@@ -33,14 +33,14 @@ class Displays:
             print_hack_hp=settings.print_hack_hp,
             hack_hp_state_q=hack_hp_state_q,
         )
-        self.ascii_picture = AsciiPicture(short_name, channels, hack_hp_state_q)
+        self.picture = AsciiPicture(short_name, channels, hack_hp_state_q)
         self.update()
 
     def update(self) -> Self:
         self.odds_and_ends.update()
         self.thermostat.update()
         self.power.update()
-        self.ascii_picture.update()
+        self.picture.update()
         return self
 
     def __rich_console__(self, _console: Console, _options: ConsoleOptions) -> RenderResult:
@@ -48,7 +48,7 @@ class Displays:
         yield self.odds_and_ends
         yield self.thermostat
         yield self.power
-        yield self.ascii_picture
+        yield self.picture
 
 
 
