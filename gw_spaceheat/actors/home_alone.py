720d681afb2b4f1475b91cd82e3ce83986b39aac
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-11-12 10:53:59 -0500

    go back to correct home alone

--- gw_spaceheat/actors/home_alone.py
+++ gw_spaceheat/actors/home_alone.py
@@ -1,50 +1,9 @@
 import asyncio
-from enum import auto
-import uuid
-import time
-from gw.enums import GwStrEnum
-from gwproactor import QOS, Actor, ServicesInterface
+from gwproactor import  Actor, ServicesInterface
 from result import Ok, Result
 from gwproto import Message
-from gwproto.message import Header
-from transitions import Machine
-from gwproto.data_classes.sh_node import ShNode
-from gwproto.data_classes.house_0_names import H0N
-from gwproto.enums import ChangeRelayState
-from gwproto.named_types import FsmEvent
-import pendulum
 
-class HomeAloneState(GwStrEnum):
-    WaitingForTemperaturesOnPeak = auto()
-    WaitingForTemperaturesOffPeak = auto()
-    HpOnStoreOff = auto()
-    HpOnStoreCharge = auto()
-    HpOffStoreOff = auto()
-    HpOffStoreDischarge = auto()
-
-    @classmethod
-    def enum_name(cls) -> str:
-        "home.alone.state"
-
-
-class HomeAloneEvent(GwStrEnum):
-    OnPeakStart = auto()
-    OffPeakStart = auto()
-    OnPeakBufferFull = auto()
-    OffPeakBufferFullStorageNotReady = auto()
-    OffPeakBufferFullStorageReady = auto()
-    OffPeakBufferEmpty = auto()
-    OnPeakBufferEmpty = auto()
-    OffPeakStorageReady = auto()
-    OffPeakStorageNotReady = auto()
-    TemperaturesAvailable = auto()
-
-    @classmethod
-    def enum_name(cls) -> str:
-        "home.alone.event"
-    
-
-class RealHomeAlone(Actor):
+class HomeAlone(Actor):
     def __init__(self, name: str, services: ServicesInterface):
         super().__init__(name, services)
         self._stop_requested: bool = False
@@ -66,7 +25,7 @@ class RealHomeAlone(Actor):
     
     async def main(self):
         while not self._stop_requested:
-            print(f"[{self.name}] In main loop. Sleeping for 5")
+            self.services.logger.error(f"[{self.name}] In main loop. Sleeping for 5")
             await asyncio.sleep(5)
 
 
