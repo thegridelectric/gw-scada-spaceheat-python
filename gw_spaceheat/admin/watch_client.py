61352c788b5a5ddf2cef8abde3a4aef7acd607cb
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-11-18 14:48:44 -0500

    starting work on admin

--- gw_spaceheat/admin/watch_client.py
+++ gw_spaceheat/admin/watch_client.py
@@ -1,5 +1,5 @@
 from typing import Any
-
+import asyncio
 import rich
 from paho.mqtt.client import Client as PahoMQTTClient
 from paho.mqtt.client import MQTTMessage
@@ -36,3 +36,24 @@ class WatchAdminClient(BaseAdminClient):
     def on_message(self, _: Any, _userdata: Any, message: MQTTMessage) -> None:
         if not self.json:
             rich.print(f"Received <{message.topic}>  in state <{self.state}>")
+        self.msg = message
+        msg_type = message.topic.split("/")[-1].replace("-", ".")
+        print(f"Just got {msg_type}")
+
+
+async def async_input(prompt: str) -> str:
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, input, prompt)
+
+class KridaUI:
+    def __init__(self):
+        self._stop_requested = False
+
+    async def krida_ui(self):
+        while not self._stop_requested:
+            i = await async_input("Pick relay number or 'E' for exit\n")
+            if i == 'E':
+                print("Exiting...")
+                self._stop_requested = True
+            else:
+                print(f"You entered: {i}")
