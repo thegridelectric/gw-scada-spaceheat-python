30399d7524a05804f911f1b5adb8a55a57ac73e0
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-12-05 17:30:30 -0500

    default seconds per snapshot is 30

--- gw_spaceheat/actors/config.py
+++ gw_spaceheat/actors/config.py
@@ -21,7 +21,7 @@ class ScadaSettings(ProactorSettings):
     local_mqtt: MQTTClient = MQTTClient()
     gridworks_mqtt: MQTTClient = MQTTClient()
     seconds_per_report: int = 300
-    seconds_per_snapshot: int = 300
+    seconds_per_snapshot: int = 30
     async_power_reporting_threshold: float = 0.02
     persister: PersisterSettings = PersisterSettings()
     admin: AdminLinkSettings = AdminLinkSettings()
