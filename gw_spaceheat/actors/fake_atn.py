ba4417abef42aafcec44953d45b459f0a56ffdd7
thdfw <th.defauw@gmail.com>
2024-12-11 15:58:17 +0100

    send_energy_instr from command line

--- gw_spaceheat/actors/fake_atn.py
+++ gw_spaceheat/actors/fake_atn.py
@@ -39,29 +39,29 @@ class FakeAtn(ScadaActor):
     async def main(self):
         await asyncio.sleep(10)
         self.log("In fake atn main loop")
-        # SendTimeMs should be no more than 10 seconds after SlotStartS
-        # slot start has to fall on top of 5 minutes
-        t = time.time()
-        slot_start_s = int(t -(t % 3600))
-        # if t - slot_start_s < 9:
-        #     sample_dispatch = EnergyInstruction(
+        # # SendTimeMs should be no more than 10 seconds after SlotStartS
+        # # slot start has to fall on top of 5 minutes
+        # t = time.time()
+        # slot_start_s = int(t -(t % 3600))
+        # # if t - slot_start_s < 9:
+        # #     sample_dispatch = EnergyInstruction(
+        # #         FromGNodeAlias=self.layout.atn_g_node_alias,
+        # #         SlotStartS=slot_start_s,
+        # #         SlotDurationMinutes=60,
+        # #         SendTimeMs=int(t * 1000),
+        # #         AvgPowerWatts=9500
+        # #     )
+        # #     self.log(f"Sample dispatch: {sample_dispatch}")
+        # sample_dispatch = EnergyInstruction(
         #         FromGNodeAlias=self.layout.atn_g_node_alias,
         #         SlotStartS=slot_start_s,
         #         SlotDurationMinutes=60,
-        #         SendTimeMs=int(t * 1000),
+        #         SendTimeMs=int(slot_start_s*1000+5000),
         #         AvgPowerWatts=9500
         #     )
-        #     self.log(f"Sample dispatch: {sample_dispatch}")
-        sample_dispatch = EnergyInstruction(
-                FromGNodeAlias=self.layout.atn_g_node_alias,
-                SlotStartS=slot_start_s,
-                SlotDurationMinutes=60,
-                SendTimeMs=int(slot_start_s*1000+5000),
-                AvgPowerWatts=9500
-            )
-        self._send_to(self.synth_generator, sample_dispatch)
-        sample_power = PowerWatts(Watts=1000)
-        self._send_to(self.synth_generator, sample_power)
+        # self._send_to(self.synth_generator, sample_dispatch)
+        # sample_power = PowerWatts(Watts=1000)
+        # self._send_to(self.synth_generator, sample_power)
 
         # MarketPriceUnit.USDPerMWh
         # # at or below $35.432/MWh buy 7 kWh
@@ -80,3 +80,20 @@ class FakeAtn(ScadaActor):
 
         while not self._stop_requested:
             await asyncio.sleep(self.MAIN_LOOP_SLEEP_SECONDS)
+
+    def send_energy_instr(self, watthours:int):
+        t = time.time()
+        wait_s = 300 - t%300
+        time.sleep(wait_s)
+        t = time.time()
+        slot_start_s = int(t - (t % 300))
+        if t - slot_start_s < 10:
+            sample_dispatch = EnergyInstruction(
+                    FromGNodeAlias=self.layout.atn_g_node_alias,
+                    SlotStartS=slot_start_s,
+                    SlotDurationMinutes=60,
+                    SendTimeMs=int(time.time()*1000),
+                    AvgPowerWatts=int(watthours)
+                )
+            self.log(f"Sent EnergyInstruction: {sample_dispatch}")
+            self._send_to(self.synth_generator, sample_dispatch)
