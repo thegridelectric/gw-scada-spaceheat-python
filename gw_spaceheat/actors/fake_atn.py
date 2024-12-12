91f89b1efbc038743ed6e4f4f90832061e2f0bc0
thdfw <th.defauw@gmail.com>
2024-12-12 17:06:47 +0100

    Adding FLO

--- gw_spaceheat/actors/fake_atn.py
+++ gw_spaceheat/actors/fake_atn.py
@@ -1,12 +1,18 @@
 from gwproactor import ServicesInterface
 import asyncio
 import time
+from datetime import datetime
 from gwproto import Message
 from result import Ok, Result
+from typing import List
 from actors.scada_actor import ScadaActor
 from gw.enums import MarketTypeName
 from enums import MarketPriceUnit, MarketQuantityUnit
-from named_types import AtnBid, EnergyInstruction, LatestPrice
+from named_types import AtnBid, EnergyInstruction, LatestPrice, Ha1Params
+from named_types.price_quantity_unitless import PriceQuantityUnitless
+from actors.scada_data import ScadaData
+from actors.flo import DGraph, DConfig, get_initial_state
+from actors.synth_generator import WeatherForecast, PriceForecast
 
 class FakeAtn(ScadaActor):
     MAIN_LOOP_SLEEP_SECONDS = 61
@@ -14,6 +20,16 @@ class FakeAtn(ScadaActor):
     def __init__(self, name: str, services: ServicesInterface):
         super().__init__(name, services)
         self._stop_requested: bool = False
+        self.weather_forecast = None
+        self.price_forecast = None
+
+    @property
+    def data(self) -> ScadaData:
+        return self._services.data
+    
+    @property
+    def params(self) -> Ha1Params:
+        return self.data.ha1_params
 
     def start(self) -> None:
         self.services.add_task(
@@ -28,54 +44,122 @@ class FakeAtn(ScadaActor):
 
     def process_message(self, message: Message) -> Result[bool, BaseException]:
         match message.Payload:
+            case WeatherForecast():
+                self.log("Recieved weather forecast")
+                self.weather_forecast = {
+                    'oat': message.Payload.OatForecast, 
+                    'ws': message.Payload.WsForecast,
+                    }
+            case PriceForecast():
+                self.log("Recieved price forecast:")
+                self.price_forecast = {
+                    'dp': message.Payload.DpForecast,
+                    'lmp': message.Payload.LmpForecsat
+                    }
+                self.log(f"{[x+y for x,y in zip(self.price_forecast['dp'], self.price_forecast['lmp'])]}")
             case LatestPrice():
                 ...
         return Ok(True)
-    
+     
     async def main(self):
-        await asyncio.sleep(10)
-        self.log("In fake atn main loop")
-        # # SendTimeMs should be no more than 10 seconds after SlotStartS
-        # # slot start has to fall on top of 5 minutes
-        # t = time.time()
-        # slot_start_s = int(t -(t % 3600))
-        # # if t - slot_start_s < 9:
-        # #     sample_dispatch = EnergyInstruction(
-        # #         FromGNodeAlias=self.layout.atn_g_node_alias,
-        # #         SlotStartS=slot_start_s,
-        # #         SlotDurationMinutes=60,
-        # #         SendTimeMs=int(t * 1000),
-        # #         AvgPowerWatts=9500
-        # #     )
-        # #     self.log(f"Sample dispatch: {sample_dispatch}")
-        # sample_dispatch = EnergyInstruction(
-        #         FromGNodeAlias=self.layout.atn_g_node_alias,
-        #         SlotStartS=slot_start_s,
-        #         SlotDurationMinutes=60,
-        #         SendTimeMs=int(slot_start_s*1000+5000),
-        #         AvgPowerWatts=9500
-        #     )
-        # self._send_to(self.synth_generator, sample_dispatch)
-        # sample_power = PowerWatts(Watts=1000)
-        # self._send_to(self.synth_generator, sample_power)
 
-        # MarketPriceUnit.USDPerMWh
-        # # at or below $35.432/MWh buy 7 kWh
-        # mtn = MarketTypeName.rt60gate5.value
-        # market_slot_name = f"e.{mtn}.{FakeAtn.P_NODE}.{slot_start_s}"
-        # bid = AtnBid(
-        #     BidderAlias=self.layout.atn_g_node_alias,
-        #     MarketSlotName=market_slot_name,
-        #     PqPairs=[PriceQuantityUnitless(PriceTimes1000=35432, QuantityTimes1000=7000)],
-        #     InjectionIsPositive=False, # withdrawing energy since load not generation
-        #     PriceUnit=MarketPriceUnit.USDPerMWh,
-        #     QuantityUnit=MarketQuantityUnit.AvgkW,
-        #     SignedMarketFeeTxn="BogusAlgoSignature"
-        # )
-        # self.log(f"Sample bid: {bid}")
+        await asyncio.sleep(10)
 
         while not self._stop_requested:
+
+            # In the last 5 minutes of the hour: make a bid for the next hour
+            if (datetime.now().minute >= 55
+                and self.weather_forecast is not None 
+                and self.price_forecast is not None):
+
+                # TODO: Find current top temperature and thermocline
+                initial_top_temp, initial_thermocline = get_initial_state(self.data.latest_channel_values)
+                
+                # Find PQ pairs using Dijkstra
+                configuration = DConfig(
+                    InitialTopTemp = initial_top_temp,
+                    InitialThermocline = initial_thermocline,
+                    DpForecastUsdMwh = self.price_forecast['dp'],
+                    LmpForecastUsdMwh = self.price_forecast['lmp'],
+                    OatForecastF = self.weather_forecast['oat'],
+                    WindSpeedForecastMph = self.weather_forecast['ws'],
+                    AlphaTimes10 = self.params.AlphaTimes10,
+                    BetaTimes100 = self.params.BetaTimes100,
+                    GammaEx6 = self.params.GammaEx6,
+                    IntermediatePowerKw = self.params.IntermediatePowerKw,
+                    IntermediateRswtF = self.params.IntermediateRswtF,
+                    DdPowerKw = self.params.DdPowerKw,
+                    DdRswtF = self.params.DdRswtF,
+                    DdDeltaTF = self.params.DdDeltaTF,
+                    MaxEwtF = self.params.MaxEwtF
+                )
+                self.log("Creating graph")
+                g = DGraph(configuration)
+                self.log("Solving Dijkstra")
+                g.solve_dijkstra()
+                pq_pairs: List[PriceQuantityUnitless] = g.generate_bid()
+                self.log(f"Obtained Price-Quantity pairs")
+
+                # Generate bid
+                t = time.time()
+                slot_start_s = int(t-(t%3600))
+                mtn = MarketTypeName.rt60gate5.value
+                market_slot_name = f"e.{mtn}.{FakeAtn.P_NODE}.{slot_start_s}"
+                bid = AtnBid(
+                    BidderAlias=self.layout.atn_g_node_alias,
+                    MarketSlotName=market_slot_name,
+                    PqPairs=pq_pairs,
+                    InjectionIsPositive=False, # withdrawing energy since load not generation
+                    PriceUnit=MarketPriceUnit.USDPerMWh,
+                    QuantityUnit=MarketQuantityUnit.AvgkW,
+                    SignedMarketFeeTxn="BogusAlgoSignature"
+                )
+                self.log(f"Bid: {bid}")
+
+            else:
+                self.log(f"Minute {datetime.now().minute}")
+
             await asyncio.sleep(self.MAIN_LOOP_SLEEP_SECONDS)
+
+
+            # # SendTimeMs should be no more than 10 seconds after SlotStartS
+            # # slot start has to fall on top of 5 minutes
+            # t = time.time()
+            # slot_start_s = int(t -(t % 3600))
+            # # if t - slot_start_s < 9:
+            # #     sample_dispatch = EnergyInstruction(
+            # #         FromGNodeAlias=self.layout.atn_g_node_alias,
+            # #         SlotStartS=slot_start_s,
+            # #         SlotDurationMinutes=60,
+            # #         SendTimeMs=int(t * 1000),
+            # #         AvgPowerWatts=9500
+            # #     )
+            # #     self.log(f"Sample dispatch: {sample_dispatch}")
+            # sample_dispatch = EnergyInstruction(
+            #         FromGNodeAlias=self.layout.atn_g_node_alias,
+            #         SlotStartS=slot_start_s,
+            #         SlotDurationMinutes=60,
+            #         SendTimeMs=int(slot_start_s*1000+5000),
+            #         AvgPowerWatts=9500
+            #     )
+            # self._send_to(self.synth_generator, sample_dispatch)
+            # sample_power = PowerWatts(Watts=1000)
+            # self._send_to(self.synth_generator, sample_power)
+
+            # MarketPriceUnit.USDPerMWh
+            # # at or below $35.432/MWh buy 7 kWh
+            # mtn = MarketTypeName.rt60gate5.value
+            # market_slot_name = f"e.{mtn}.{FakeAtn.P_NODE}.{slot_start_s}"
+            # bid = AtnBid(
+            #     BidderAlias=self.layout.atn_g_node_alias,
+            #     MarketSlotName=market_slot_name,
+            #     PqPairs=[PriceQuantityUnitless(PriceTimes1000=35432, QuantityTimes1000=7000)],
+            #     InjectionIsPositive=False, # withdrawing energy since load not generation
+            #     PriceUnit=MarketPriceUnit.USDPerMWh,
+            #     QuantityUnit=MarketQuantityUnit.AvgkW,
+            #     SignedMarketFeeTxn="BogusAlgoSignature"
+            # )
+            # self.log(f"Sample bid: {bid}")
 
     def send_energy_instr(self, watthours:int):
         t = time.time()
