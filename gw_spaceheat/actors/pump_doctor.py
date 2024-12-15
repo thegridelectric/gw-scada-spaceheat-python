b93a6146031b6f9dd4d64b96fc01c35b33aac2dd
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-12-15 08:57:53 -0500

    wip pump doctor

--- gw_spaceheat/actors/pump_doctor.py
+++ gw_spaceheat/actors/pump_doctor.py
@@ -1,12 +1,9 @@
 import asyncio
 import time
-from typing import Sequence
-from gwproactor import MonitoredName, ServicesInterface
-from gwproactor.message import PatInternalWatchdogMessage
+from gwproactor import ServicesInterface
 from gwproto import Message
 from result import Ok, Result
 from actors.scada_actor import ScadaActor
-from data_classes.house_0_names import H0CN
 
 from named_types import GoDormant, WakeUp
 
@@ -18,16 +15,10 @@ class PumpDoctor(ScadaActor):
         self._stop_requested: bool = False
 
     def start(self) -> None:
-        self.services.add_task(
-            asyncio.create_task(self.main(), name="")
-        )
-
-    @property
-    def monitored_names(self) -> Sequence[MonitoredName]:
-        return [MonitoredName(self.name, 120)]
+        ...
     
     def stop(self) -> None:
-        self._stop_requested = True
+        ...
         
     async def join(self):
         ...
@@ -39,10 +30,3 @@ class PumpDoctor(ScadaActor):
             case WakeUp():
                 ...
         return Ok(True)
-
-    async def main(self):
-        await asyncio.sleep(2)
-        last_pat = time.time()
-        while not self._stop_requested:
-
-            self._send(PatInternalWatchdogMessage(src=self.name))
