3572ee8fcb870c775b7bd9e933f31ecb6613c82b
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-12-18 19:22:07 -0500

    Energy Instruction has axioms

--- gw_spaceheat/named_types/energy_instruction.py
+++ gw_spaceheat/named_types/energy_instruction.py
@@ -1,9 +1,10 @@
 """Type energy.instruction, version 000"""
 
 from typing import Literal
+from typing_extensions import Self
 
 from gwproto.property_format import LeftRightDotStr, UTCMilliseconds, UTCSeconds
-from pydantic import BaseModel, PositiveInt, StrictInt, field_validator
+from pydantic import BaseModel, PositiveInt, StrictInt, field_validator, model_validator
 
 
 class EnergyInstruction(BaseModel):
@@ -21,5 +22,17 @@ class EnergyInstruction(BaseModel):
         """
         Axiom 1: SlotStartS should fall on the top of 5 minutes
         """
-        # Implement Axiom(s)
+        if v % 300 != 0:
+            raise ValueError(f"Axiom 1: SlotStartS should fall on the top of 5 minutes. But got {v % 300} seconds out")
         return v
+
+    @model_validator(mode="after")
+    def check_axiom_2(self) -> Self:
+        """
+        Axiom 2: SendTime within 10 seconds of SlotStart.
+        SendTimeMs should be no more than 10 seconds after SlotStartS
+        """
+        send_time_s = self.SendTimeMs / 1000
+        if send_time_s > self.SlotStartS + 10:
+            raise ValueError(f"Axiom 2: SendTime within 10 seconds of SlotStart. Got {round(send_time_s - self.SlotStartS, 2)}")
+        return self
