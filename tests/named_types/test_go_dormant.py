57eb4fa23facd2346dee001f84db5044611b13d5
Jessica Millar <jessica.lynn.millar@gmail.com>
2025-02-21 21:15:15 -0500

    tests pass

--- tests/named_types/test_go_dormant.py
+++ tests/named_types/test_go_dormant.py
@@ -5,11 +5,9 @@ from named_types import GoDormant
 
 def test_go_dormant_generated() -> None:
     d = {
-        "FromName": "auto",
         "ToName": "pico-cycler",
-        "TriggerId": "dc6c6689-e1b1-4e60-adf9-94db59c7f96a",
         "TypeName": "go.dormant",
-        "Version": "000",
+        "Version": "001",
     }
 
     d2 = GoDormant.model_validate(d).model_dump(exclude_none=True)
